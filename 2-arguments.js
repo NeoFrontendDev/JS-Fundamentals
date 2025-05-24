13d6ee4289fa3ac2d00bdfc0e2d7ea4081b5bd1c
Neo Mosebi <neosammosebi01@gmail.com>
2025-05-25 01:09:52 +0200

    Add files via upload

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const arguments = arguments.process.slice(2);
+
+if (arguments.length === 0) {
+    console.log("No argument");
+}
+else if (arguments.length === 1) {
+    console.log("Best argument found");
+}
+else {console.log("Best School Arguments found");
+}
