fe76ea516f7016dab2912ed3a8f1e67960173eaf
Neo Mosebi <neosammosebi01@gmail.com>
2025-05-25 00:55:23 +0200

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,6 @@
 let language1 = "C is fun";
 let language2 = "Python is cool";
-let language3 = "JavaScript is amzing";
+let language3 = "JavaScript is amazing";
 
 console.log(language1);
 console.log(language2);
